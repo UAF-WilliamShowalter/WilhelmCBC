55f15730a3d0372208d23e5c886c235ed9cb08bb
William Showalter <williamshowalter@gmail.com>
2013-02-23 21:05:26 -0900

    Initial Commit

---
+++ WilhelmCBC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WilhelmCBC
+//
+//  Created by William Showalter on 2/23/13.
+//  Copyright (c) 2013 William Showalter. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
